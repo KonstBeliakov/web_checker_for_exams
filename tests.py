199365e81da632a197ac1b420ce80a9a495e8d34
KonstBeliakov <konstbeliakov@gmail.com>
2024-12-26 02:11:59 +0300

    tests.py updated

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 
 def check_route_exists():
@@ -7,11 +8,15 @@ def check_route_exists():
         response = requests.get(url)
         if response.status_code == 200:
             print("Test passed: Status code is 200")
+            return 0
         else:
             print(f"Test failed: Status code is {response.status_code}")
+            return -1
     except requests.exceptions.RequestException as e:
         print(f"Test failed: {e}")
+        return -1
 
 
 if __name__ == '__main__':
-    check_route_exists()
+    if t := check_route_exists():
+        sys.exit(t)
